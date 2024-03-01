9e2f407eb2705c425c01a5d05960049148bac694
Amirul <muh.amirul.ihsan@gmail.com>
2024-03-01 14:21:01 +0700

    init & migration

---
+++ database/migrations/2024_03_01_071517_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')->constrained();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('cover_image')->nullable()->default(null);
+            $table->string('description');
+            $table->string('year', 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
